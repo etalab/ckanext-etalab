cb0be05583c2178b1c2b1157f27d5d4d1e9c3cf7
Emmanuel Raviart <eraviart@easter-eggs.com>
2013-07-04 16:10:27 +0200

    Improve home page.

--- ckanext/etalab/theme.py
+++ ckanext/etalab/theme.py
@@ -11,14 +11,12 @@ class EtalabThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
 
-    # Update CKAN's config settings, see the IConfigurer plugin interface.
-    def update_config(self, config):
-
-        # Tell CKAN to use the template files in
-        # ckanext/example_itemplatehelpers/templates.
-        plugins.toolkit.add_template_directory(config, 'templates')
-
-    # Tell CKAN what custom template helper functions this plugin provides,
-    # see the ITemplateHelpers plugin interface.
     def get_helpers(self):
+        # Tell CKAN what custom template helper functions this plugin provides,
+        # see the ITemplateHelpers plugin interface.
         return {'example_helper': example_helper}
+
+    def update_config(self, config):
+        # Update CKAN's config settings, see the IConfigurer plugin interface.
+        plugins.toolkit.add_public_directory(config, 'public')
+        plugins.toolkit.add_template_directory(config, 'templates')
