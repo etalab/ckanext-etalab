7e095331affee82ca84da01475f4adfc38224009
Emmanuel Raviart <eraviart@easter-eggs.com>
2013-08-16 10:51:26 +0200

    Cleanup setup.py.

--- setup.py
+++ setup.py
@@ -24,34 +24,56 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from setuptools import setup, find_packages
+"""CKAN extension for data.gouv.fr site"""
+
+
 import sys, os
 
-version = ''
+from setuptools import setup, find_packages
+
+
+classifiers = """\
+Development Status :: 2 - Pre-Alpha
+Environment :: Plugins
+Environment :: Web Environment
+Intended Audience :: Developers
+Intended Audience :: System Administrators
+License :: OSI Approved :: GNU Affero General Public License v3
+Operating System :: POSIX
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+"""
+
+doc_lines = __doc__.split('\n')
+
+version = '0.1'
+
 
 setup(
     name = 'ckanext-etalab',
     version = version,
-    description = "CKAN extension for data.gouv.fr site",
-    long_description = """\
-    """,
-    classifiers = [], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords = '',
+
     author = 'Emmanuel Raviart',
     author_email = 'emmanuel@raviart.com',
-    url = '',
-    license = 'AGPL',
-    packages = find_packages(exclude = ['ez_setup', 'examples', 'tests']),
-    namespace_packages = ['ckanext', 'ckanext.etalab'],
-    include_package_data = True,
-    zip_safe = False,
-    install_requires = [
-        # -*- Extra requirements: -*-
-    ],
+    classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
+    description = doc_lines[0],
+    keywords = 'ckan etalab extension france government opendata plugin',
+    license = 'http://www.fsf.org/licensing/licenses/agpl-3.0.html',
+    long_description = '\n'.join(doc_lines[2:]),
+    url = 'https://www.gitorious.org/etalab/ckanext-etalab',
+
     entry_points = """
         [ckan.plugins]
         etalab = ckanext.etalab.plugins:EtalabPlugin
         etalab_dataset_form = ckanext.etalab.plugins:EtalabDatasetFormPlugin
         etalab_query = ckanext.etalab.plugins:EtalabQueryPlugin
         """,
+    include_package_data = True,
+    install_requires = [
+        'ckan',
+        ],
+    namespace_packages = ['ckanext'],
+    packages = find_packages(exclude = ['ez_setup']),
+    zip_safe = False,
     )
